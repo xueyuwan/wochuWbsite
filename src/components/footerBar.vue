6f2ac4fff9c581c415262ff3ec9a4d28877ae0eb
xueyuwan <1213287572@qq.com>
2017-09-14 17:59:48 +0800

    picture

--- src/components/footerBar.vue
+++ src/components/footerBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="background:#000;color:#fff;margin-top:30px;">
       footerbar
   </div>
 </template>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,17 +2,9 @@
   <div>
     <div class="block">
       <el-carousel height="500px">
-        <el-carousel-item>
-          <img src="./../images/banner1.jpg">
+        <el-carousel-item v-for="item in banner">
+          <img :src="item.img" alt="">
         </el-carousel-item>
-        <el-carousel-item>
-          <img src="./../images/banner2.jpg">
-        </el-carousel-item>
-
-        <!-- <el-carousel-item :for="item in banner">
-            <img :src="item.img" alt="">
-          </el-carousel-item>  -->
-
       </el-carousel>
     </div>
     <div>
@@ -41,17 +33,18 @@
               <a href="">更多 >></a>
             </p>
           </div>
-          <!-- <div class="goodsRight right">
-                      <el-row>
-                        <el-col :span="6" :for="item in goods">
-                          <img :src="item.img" alt="">
-                          <p>{{item.title}}</p>
-                          <p>{{item.originalPrice}}</p>
-                          <p>{{item.presentPrice}}</p>
-                        </el-col>
+          <div class="goodsRight right">
+            <el-row>
+              <el-col v-for="item in goods" class="goods-item" :span="6">
+                <img :src="item.img" alt="">
+                <p>{{item.title}}</p>
+                <p>{{item.originalPrice}}</p>
+                <p>{{item.presentPrice}}</p>
+                <div><img src="./../images/addcart.png" alt=""></div>
+              </el-col>
 
-                      </el-row>
-                </div> -->
+            </el-row>
+          </div>
         </div>
       </div>
 
@@ -62,16 +55,34 @@
 
 </style>
 <script>
+import banner1 from './../images/banner1.jpg';
+import banner2 from './../images/banner2.jpg';
+import pic1 from './../images/goods1.jpg';
+import pic2 from './../images/goods2.jpg';
+import pic3 from './../images/goods3.jpg';
+import pic4 from './../images/goods4.jpg';
+import pic5 from './../images/goods5.jpg';
+import pic6 from './../images/goods6.jpg';
+import pic7 from './../images/goods7.jpg';
+import pic8 from './../images/goods8.jpg';
+import addtocar from './../images/addcart.png';
 export default {
   data() {
     return {
       banner: [
-        { "img": "./images/banner1.jpg" },
-        { "img": "./images/banner2.jpg" }
+        { "img": banner1 },
+        { "img": banner2 }
       ],
-      // goods: [
-      // { "img": "./../images/goods1.jpg" ,"title":"小青菜350g","originalPrice":"3.99","presentPrice":"1.99"},
-      // { "img": "./../images/goods2.jpg" ,"title":"小青菜350g","originalPrice":"3.99","presentPrice":"1.99"}]
+      goods: [
+        { "img": pic1, "title": "小青菜350g", "originalPrice": "3.99", "presentPrice": "1.99" },
+        { "img": pic2, "title": "小青菜350g", "originalPrice": "3.99", "presentPrice": "1.99" },
+        { "img": pic3, "title": "小青菜350g", "originalPrice": "3.99", "presentPrice": "1.99" },
+        { "img": pic4, "title": "小青菜350g", "originalPrice": "3.99", "presentPrice": "1.99" },
+        { "img": pic5, "title": "小青菜350g", "originalPrice": "3.99", "presentPrice": "1.99" },
+        { "img": pic6, "title": "小青菜350g", "originalPrice": "3.99", "presentPrice": "1.99" },
+        { "img": pic7, "title": "小青菜350g", "originalPrice": "3.99", "presentPrice": "1.99" },
+        { "img": pic8, "title": "小青菜350g", "originalPrice": "3.99", "presentPrice": "1.99" },
+      ]
     }
   }
 }
@@ -88,6 +99,7 @@ export default {
 .contentBox {
   width: 1280px;
   margin: 0 auto;
+  overflow: auto;
   .el-row {
     margin-top: 20px;
     height: 164px;
@@ -118,9 +130,25 @@ export default {
       .el-row {
         .el-col {
           height: 310px;
+          position: relative;
+          p {
+            text-align: left;
+            text-indent: 20px;
+          }
           img {
-            width: 224px;
+            width: 100%;
             height: 224px;
+          }
+          div {
+            position: absolute;
+            right: 10px;
+            bottom: 10px;
+            width: 36px;
+            height: 36px;
+            img {
+              width: 36px;
+              height: 36px;
+            }
           }
         }
       }
